0aaca7f09ba34303bde60365b9db734b6f4e5505
Nattapong Nantasang <comford55garena@gmail.com>
2023-03-30 23:25:36 +0700

    add new slang data

--- GetOldTweets3-0.0.10/regexCharacter.py
+++ GetOldTweets3-0.0.10/regexCharacter.py
@@ -11,8 +11,8 @@ def cleanText(tweets):
         clean.append(regex)
     return clean
 
-def saveToCsv(list):
-    with open('Phueak100kCleanUp.csv', 'w', newline='',encoding="UTF-8") as myfile:
+def saveToCsv(list, name):
+    with open(f'cleanUp-{name}.csv', 'w', newline='',encoding="UTF-8") as myfile:
         writer = csv.writer(myfile, delimiter=",")
         writer.writerow(['tweet'])
         for row in list:
@@ -26,7 +26,25 @@ def saveToCsv(list):
 # cleanTweets = cleanText(tweets)
 # saveToCsv(cleanTweets)
 
-df = pd.read_csv("thai-language-tweets-phueak.csv")
+# def saveMultipleToCsv():
+#     name = ['เฉียบ', 'เม้าท์', 'เม้า', 'แห้ว', 'กาก', 'จกตา', 'จึ้ง', 'ซิง', 'ดือ', 'ติส', 'บ้ง', 'บิด', 'บูด', 'ปัง', 'มงลง', 'มองแรง', 'มู', 'ลำไย', 'สับ', 'หยอง']
+    
+#     for i in name:
+#         df = pd.read_csv(f"E:/Optimized-Modified-GetOldTweets3-OMGOT/GetOldTweets3-0.0.10/slangwords/snscrape_words/thai-language-tweets-{i}.csv")
+#         tweet = df['content']
+#         cleanTweets = cleanText(tweet)
+#         with open(f'cleanUp-{i}.csv', 'w', newline='',encoding="UTF-8") as myfile:
+#             writer = csv.writer(myfile, delimiter=",")
+#             writer.writerow(['tweet'])
+#             for row in cleanTweets:
+#                 columns = [c.strip() for c in row.strip(', ').split(',')]
+#                 writer.writerow(columns)
+
+
+name = ['เฉียบ', 'เม้าท์', 'เม้า', 'แห้ว', 'กาก', 'จกตา', 'จึ้ง', 'ซิง', 'ดือ', 'ติส', 'บ้ง', 'บิด', 'บูด', 'ปัง', 
+        'มงลง', 'มองแรง', 'มู', 'ลำไย', 'สับ', 'หยอง']
+
+df = pd.read_csv("E:/Optimized-Modified-GetOldTweets3-OMGOT/GetOldTweets3-0.0.10/slangwords/snscrape_words/thai-language-tweets-หยอง.csv")
 tweets = df['content'].dropna()
 cleanTweets = cleanText(tweets)
-saveToCsv(cleanTweets)
+saveToCsv(cleanTweets, 'หยอง')
